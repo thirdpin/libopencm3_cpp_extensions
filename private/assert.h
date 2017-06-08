311ee1a76fb2bc51e28fcc4454a4681665ef8daf
Dmitriy Lisin <mrlisdim@gmail.com>
2017-06-08 21:46:54 +0300

    IMPR: [assert] More straight logic of using "#if"

--- private/assert.h
+++ private/assert.h
@@ -3,14 +3,14 @@
 
 #include <cm3ext_config.h>
 
-#ifndef CM3_ASSERT
-#define CM3_ASSERT( x ) if( ( x ) == 0 ) { while(true) { asm(""); } }
+#ifndef CM3EXT_ASSERT
+#define CM3EXT_ASSERT( x ) if( ( x ) ) { while(true) { asm(""); } }
 #endif
 
-#if CM3EXT_ENABLE_IMPLISIT_DESTRUCTOR_CALLS == 0
-#define CM3EXT_EXPLISIT_DESTRUCTOR(c) ~c() { CM3_ASSERT(false) }
-#else
+#if CM3EXT_ENABLE_IMPLISIT_DESTRUCTOR_CALLS == 1
 #define CM3EXT_EXPLISIT_DESTRUCTOR(c) ~c() = default;
+#else
+#define CM3EXT_EXPLISIT_DESTRUCTOR(c) ~c() { CM3EXT_ASSERT(false) }
 #endif
 
 #endif /* LIBOPENCM3_CPP_EXTENSIONS_ASSERT_H_ */
