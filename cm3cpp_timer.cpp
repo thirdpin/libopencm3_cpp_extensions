cbaa3d85283d6da35ab2b5551c28426ebe59c5e5
ana_lazareva <n_lazareva@mail.ru>
2018-04-04 18:06:17 +0300

    MISC: [cm3cpp_timer] get_counter_value32() removed because it's useless

--- cm3cpp_timer.cpp
+++ cm3cpp_timer.cpp
@@ -1641,11 +1641,6 @@ uint16_t Timer::get_counter_value()
 	return TIM_CNT(_timer);
 }
 
-uint32_t Timer::get_counter_value32()
-{
-	return TIM_CNT(_timer);
-}
-
 //9,12
 void Timer::set_counter_value(uint16_t value)
 {
